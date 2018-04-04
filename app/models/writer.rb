af163e684cabf6903328fbecb707821cc5f2db56
Yassir <hartaniyassir@gmail.com>
2018-04-04 10:00:00 +0100

    add login form

--- app/models/writer.rb
+++ app/models/writer.rb
@@ -1,11 +1,10 @@
 class Writer < ApplicationRecord
     before_validation :downcase_email
     validates :email, :uniqueness => { :case_sensitive => false }
-    validates_presence_of :name , :email 
+    validates :name, presence: true, length: { minimum: 2, maximum: 30}
     validates_email_format_of :email
     has_secure_password
     validates :password, length: { minimum: 8, maximum: 15} 
-    validates :name, length: { minimum: 2, maximum: 30}
   
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   resources :writers
+  resources :sessions, only: [:create, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
